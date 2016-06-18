a37dde2b5d8a102057dfd60efa6611d9e8f2f734
tekhe <tekhe@DESKTOP-41SLNS5>
2016-06-18 09:21:14 -0400

    Add project files.

---
+++ Master/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Master
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string assemPath = Assembly.GetExecutingAssembly().Location;
+            int idx = assemPath.IndexOf("DynamicAssemblyTest");
+            assemPath = Path.Combine(assemPath.Substring(idx), "ToLoad\\bin\\Debug\\ToLoad.exe");
+
+            // Assembly loadedAssembly = Assembly.LoadFile(@"........\DynamicAssemblyTest\ToLoad\bin\Debug\ToLoad.exe");
+            byte[] assemblyAsBytes = File.ReadAllBytes(@"C:\Users\tekhe\Documents\visual studio 2015\Projects\DynamicAssemblyTest\ToLoad\bin\Debug\ToLoad.exe");
+            Assembly loadedAssembly = AppDomain.CurrentDomain.Load(assemblyAsBytes);
+            MethodInfo mi = loadedAssembly.EntryPoint;
+
+            System.Console.WriteLine("I am the Master");
+
+            //var obj = loadedAssembly.CreateInstance(loadedAssembly.GetType().Name);
+            var obj = loadedAssembly.CreateInstance(mi.Name);
+            mi.Invoke(obj, null);
+
+            Console.WriteLine("Its done, but I'm still running.");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ToLoad/Servant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ToLoad
+{
+    public class Servant
+    {
+        public static void Main()
+        {
+            DelayMe().Wait();
+        }
+
+        private async static Task DelayMe()
+        {
+            await Task.Delay(3000);
+            Console.WriteLine("Hello from the loaded assembly.");
+        }
+    }
+}
